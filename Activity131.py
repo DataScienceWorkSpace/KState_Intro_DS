6784bccefb19f663a97c2374252d73034177ada6
pSirigala <psirigala@gmail.com>
2022-02-15 19:14:42 -0600

    Create Activity131.py
    Activity 131

---
+++ Activity131.py
@@ -0,0 +1,20 @@
+
+num_latte = 10
+num_mocha = 7
+num_coffee = 12
+
+price_latte = 4
+price_mocha = 4.25
+price_coffee = 2
+
+total_latte = num_latte * price_latte
+print(total_latte)
+
+total_mocha  = num_mocha * price_mocha
+print(total_mocha)
+
+total_coffee  = num_coffee * price_coffee
+print(total_coffee)
+
+grand_total = total_latte + total_mocha + total_coffee
+print(grand_total)
